36959bff8c333757dd4b9adf4f4863d0dfc7d73e
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-12-01 11:22:01 -0300

    Exercicios realizados no dia 01/12/2021

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,10 +1,18 @@
 import Header from "../../components/Header/Header";
+import { useContext } from "react";
+import { GlobalContext } from "../../contexts/GlobalContext";
 
 function NotFoundPage(props) {
+
+  const context = useContext(GlobalContext);
+
+  const {bananinha} = context
+
   return (
     <div>
       <Header />
       <h1>Ops! Essa página foi levada pela Equipe Rocket!</h1>
+      <h2>{bananinha}</h2>
     </div>
   );
 }
